f1f7414fe5c1e9933513cbbcd808758d7f031c27
raafat30 <raafatzid39@gmail.com>
2021-04-27 15:34:55 +0100

    new project

---
+++ js/plugins.js
@@ -0,0 +1,17 @@
+var mainPrice = 100,
+  mySmallDis = 20,
+  myMediumDis = 40,
+  myBigDis = 50;
+
+document.getElementById('price1').innerHTML = mainPrice;
+document.getElementById('price2').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price3').innerHTML = mainPrice - mySmallDis + "$";
+document.getElementById('price4').innerHTML = mainPrice - myBigDis + "$";
+document.getElementById('price5').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price6').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price7').innerHTML = mainPrice - mySmallDis + "$";
+document.getElementById('price8').innerHTML = mainPrice - myBigDis + "$";
+document.getElementById('price9').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price10').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price11').innerHTML = mainPrice - myMediumDis + "$";
+document.getElementById('price12').innerHTML = mainPrice - myMediumDis + "$";
